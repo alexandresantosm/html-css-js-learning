9fc9f48cc38a3d9ad6c404dce72d7f7e3d5180f5
Alexandre Menezes <amenezes031@gmail.com>
2022-05-27 17:31:11 -0300

    feat(main.js): close menu with click about the close menu button

--- page-not-found/src/assets/js/main.js
+++ page-not-found/src/assets/js/main.js
@@ -1,6 +1,7 @@
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu')
 const navToggleMenu = document.getElementById('nav-toggle-menu')
+const navMenuClose = document.getElementById('nav-menu-close')
 
 /*========= OPEN MENU =========*/
 if (navToggleMenu) {
@@ -9,4 +10,13 @@ if (navToggleMenu) {
 
 function openMenu() {
   navMenu.classList.add('show-menu')
+}
+
+/*========= CLOSE MENU =========*/
+if (navMenuClose) {
+  navMenuClose.addEventListener('click', closeMenu)
+}
+
+function closeMenu() {
+  navMenu.classList.remove('show-menu')
 }
