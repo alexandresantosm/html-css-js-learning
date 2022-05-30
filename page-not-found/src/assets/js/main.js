72af2e15c91f774188758e301376a153f55a3a18
Alexandre Menezes <amenezes031@gmail.com>
2022-05-30 17:37:11 -0300

    feat(main.js): add animated effects to text, image and footer elements

--- page-not-found/src/assets/js/main.js
+++ page-not-found/src/assets/js/main.js
@@ -31,3 +31,13 @@ navMenuLinks.forEach(itemMenuLink =>
 function menuLinkAction() {
   closeMenu()
 }
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const scrollReveal = ScrollReveal({
+  distance: '90px',
+  duration: 3000
+})
+
+scrollReveal.reveal('.home__data', { origin: 'top', delay: 400 })
+scrollReveal.reveal('.home__img', { origin: 'bottom', delay: 600 })
+scrollReveal.reveal('.home__footer', { origin: 'bottom', delay: 800 })
